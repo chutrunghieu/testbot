5c6158245e4ee002c9117adf47f4819cf84bfece
chutrunghieu12 <chutrunghieu12@gmail.com>
2025-10-29 17:04:56 +0700

    try catch

--- src/job/application/services/send-message.job.ts
+++ src/job/application/services/send-message.job.ts
@@ -10,11 +10,18 @@ export class SendMessageCommand {
 
   @Cron('* * * * *')
   async handleCron() {
-    console.log('Sending scheduled message to Telegram...');
-    await this.botService.sendMessage(
-      process.env.CHAT_ID!,
-      'Scheduled message from NestJS Bot!',
-    );
-    this.logger.log('Sent scheduled message to Telegram');
+    try {
+      console.log('Sending scheduled message to Telegram...');
+      await this.botService.sendMessage(
+        process.env.CHAT_ID!,
+        'Scheduled message from NestJS Bot!',
+      );
+      this.logger.log('Sent scheduled message to Telegram');
+    } catch (error) {
+      this.logger.error(
+        'Error sending scheduled message to Telegram',
+        error.stack,
+      );
+    }
   }
 }
